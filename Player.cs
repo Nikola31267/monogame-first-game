6fe3eec25bdb628da9341b64794821d3f9323c16
Nikola31267 <nikola.minchev13@gmail.com>
2026-04-15 09:44:18 +0300

    adding player file

---
+++ Player.cs
@@ -0,0 +1,9 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Player
+{
+    public Vector2 Position;
+    public Vector2 Size;
+}
